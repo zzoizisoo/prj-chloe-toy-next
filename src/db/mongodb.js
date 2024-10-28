d12b61fbd0842fd6c921324266d65a9b49ae6d5a
Chloe <zzoizisoo@gmail.com>
2024-10-28 18:01:16 +0900

    mongoClient setting

---
+++ src/db/mongodb.js
@@ -0,0 +1,30 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URI; // Your MongoDB connection string
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
+
+global._mongoClientPromise = global._mongoClientPromise || null;
+
+class Singleton {
+  constructor() {
+    if (!global._mongoClientPromise) {
+      const client = new MongoClient(uri, options);
+      global._mongoClientPromise = client.connect();
+    }
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Singleton();
+    }
+    return global._mongoClientPromise;
+  }
+}
+
+export const getMongoClient = Singleton.getInstance();
